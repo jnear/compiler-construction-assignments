a9906727e83cd0cd2c5cf0a484b8b96e2346aa38
Joe Near <jnear@uvm.edu>
2019-02-03 20:53:17 -0500

    modify allowed registers to be just the callee-saved ones

--- compiler-A2.rkt
+++ compiler-A2.rkt
@@ -162,7 +162,7 @@
 
 ;; assign-homes : homes -> pseudo-x86 -> pseudo-x86
 (define (assign-homes e)
-  (define all-registers '(r8 r9 r10 r11 r12 r13 r14 r15))
+  (define all-registers '(rbp rbx r12 r13 r14 r15))
 
   (define (as* ss homes)
     (map (as-instr homes) ss))

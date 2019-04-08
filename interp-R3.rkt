af6eaea1563d7356b08f234c3948ddcf37435f26
Joe Near <jnear@uvm.edu>
2019-04-08 19:09:21 -0400

    updated (correct versions!) of interp-x86, and runtime-config

--- interp-R3.rkt
+++ interp-R3.rkt
@@ -1,6 +1,8 @@
 #lang racket
 (require racket/fixnum)
+(require racket/trace)
 (require "utilities.rkt")
+(require (prefix-in runtime-config: "runtime-config.rkt"))
 (provide interp-R3 interp-C2 interp-x86)
 
 ;; Note to maintainers of this code:
@@ -125,7 +127,41 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (interp-x86 e)
+  (define memory          '())
+  (define stack-size      (runtime-config:rootstack-size))
+  (define heap-size       (runtime-config:heap-size))
+  (define uninitialized   'uninitialized-value-from-memory)
+  (define fromspace_begin (box uninitialized))
+  (define rootstack_end   (box uninitialized))
+  (define free_ptr	  (box uninitialized))
+  (define fromspace_end   (box uninitialized))
+  (define rootstack_begin (box uninitialized))
 
+  (define (memory-set! addr val)
+    (set! memory (cons `(,addr . ,val) memory)))
+
+  (define (memory-get addr)
+    (cond
+     [(assq addr memory) => cdr]
+     [else uninitialized]))
+
+  (define (initialize! stack-length heap_length)
+    (set! memory '())
+    (let* ([s-begin 0]
+           [h-begin (+ stack-length 8)])
+      (set-box! rootstack_begin s-begin)
+      (set-box! rootstack_end   (+ s-begin stack-size))
+      (set-box! fromspace_begin h-begin)
+      (set-box! fromspace_end   (+ h-begin heap-size))
+      (set-box! free_ptr	h-begin)))
+
+  (define (collect! rootstack bytes-requested)
+    (let ([difference (- (+ (unbox free_ptr) bytes-requested) (unbox fromspace_end))])
+      (when (<= difference 0)
+            (error 'collect! "No collection was needed"))
+      (set-box! fromspace_end (+ (unbox fromspace_end) difference))
+    ))
+  
   (define byte2full-reg
     (lambda (r)
       (match r
@@ -163,12 +199,51 @@
        (interp-instr ss cfg env)]
       [`,ss (interp-instr ss cfg env)]))
 
+  (define (interp-store dest env val)
+    (match dest
+      [`(global-value rootstack_begin) 
+       (set-box! rootstack_begin val)
+       env]
+      [`(global-value rootstack_end)
+       (set-box! rootstack_end val)
+       env]
+      [`(global-value fromspace_begin) 
+       (set-box! fromspace_begin val)
+       env]
+      [`(global-value fromspace_end) 
+       (set-box! fromspace_end val)
+       env]
+      [`(global-value free_ptr) 
+       (set-box! free_ptr val)
+       env]
+
+      [`(global-value ,v) (error "unknown global value: ~a" v)]
+
+      [`(deref ,r ,i) #:when (not (eq? r 'rbp))
+       (let* ([base (interp-arg `(reg ,r) env)]
+              [addr (+ base i)])
+         (memory-set! addr val)
+         env)]
+      [dest
+       (let ([name (get-name dest)])
+         (cons `(,name . ,val) env))]))
+
   (define (interp-instr instrs cfg env)
     (match instrs
-      ['((retq)) 
+      ['((retq))
+       ;(printf "Memory contents: ~a\n" memory)
        (lookup '(reg rax) env)]
       [`((xorq (int 1) ,e) . ,ss)
        (interp-instr ss cfg (cons `(,e . ,(if (eq? (interp-arg e env) 1) 0 1)) env))]
+      [`((callq initialize) . ,ss)
+       (initialize! (runtime-config:rootstack-size)
+                    (runtime-config:heap-size))
+       (interp-instr ss cfg env)]
+      [`((callq collect) . ,ss)
+       (let ([rootstack (interp-arg '(reg rdi) env)]
+             [bytes-requested (interp-arg '(reg rsi) env)])
+         (collect! rootstack bytes-requested)
+         (interp-instr ss cfg env))]
       [`((callq read_int) . ,ss) 
        (interp-instr ss cfg (cons `((reg rax) . ,(read)) env))]
       [`((set ,cc ,d) . ,ss)
@@ -198,21 +273,35 @@
       [`((jmp ,l) . ,ss)
        (interp-block l cfg env)]
       [`((negq ,e) . ,ss) 
-       (interp-instr ss cfg (cons `(,e . ,(- (interp-arg e env))) env))]
-      [`((movq ,e₁ ,e₂) . ,ss) 
-       (interp-instr ss cfg (cons `(,e₂ . ,(interp-arg e₁ env)) env))]
-      [`((addq ,e₁ ,e₂) . ,ss) 
-       (interp-instr ss cfg (cons `(,e₂ . ,(+ (interp-arg e₁ env) (interp-arg e₂ env))) env))]))
+       (let* ([v (interp-arg e env)]
+              [new-env (interp-store e env (- v))])
+         (interp-instr ss cfg new-env))]
+      [`((movq ,e₁ ,e₂) . ,ss)
+       (let ([new-env (interp-store e₂ env (interp-arg e₁ env))])
+         (interp-instr ss cfg new-env))]
+      [`((addq ,e₁ ,e₂) . ,ss)
+       (let* ([v₁ (interp-arg e₁ env)]
+              [v₂ (interp-arg e₂ env)]
+              [new-env (interp-store e₂ env (+ v₁ v₂))])
+         (interp-instr ss cfg new-env))]))
 
   (define (interp-arg arg env)
     (match arg
       [`(int ,n) n]
       [`(var ,x) (lookup `(var ,x) env)]
       [`(reg ,r) (lookup `(reg ,r) env)]
-      [`(deref ,r ,o) (lookup arg env)]
-      [`(global-value free_ptr) 0]
-      [`(global-value fromspace_end) 100000]))
+      [`(deref bp ,o) (lookup arg env)]
+      [`(deref ,r ,o)
+       (let ([result (memory-get (+ (lookup `(reg ,r) env) o))])
+         (when (eq? result uninitialized)
+               (printf "Warning: dereferenced uninitialized memory!\n"))
+         result)]
+      [`(global-value free_ptr) (unbox free_ptr)]
+      [`(global-value fromspace_end) (unbox fromspace_end)]))
 
   (match e
-    [`(program ,_ ,blocks) 
-     (interp-block 'start blocks '())]))
+    [`(program ,_ ,blocks)
+     (initialize! (runtime-config:rootstack-size)
+                  (runtime-config:heap-size))
+     (let ([env `(((reg r15) . ,(unbox rootstack_begin)))])
+       (interp-block 'start blocks env))]))
